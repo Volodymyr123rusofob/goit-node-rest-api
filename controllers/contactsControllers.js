4e4c4f6c6347fef26b36c935618c6ddc3a31d479
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2024-03-11 22:26:48 +0200

    task-2

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,53 @@
-import contactsService from "../services/contactsServices.js";
+import {
+    listContacts,
+    getContactById,
+    // addContact,
+    removeContact,
+} from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+import HttpError from "../helpers/HttpError.js";
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+    try {
+        res.json({
+            status: "success",
+            code: 200,
+            data: {
+                contact: await listContacts(),
+            },
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const oneContact = await getContactById(id);
+        if (!oneContact) {
+            throw HttpError(404, "Not found");
+        }
+        res.json({
+            status: "success",
+            code: 200,
+            data: {
+                oneContact,
+            },
+        });
+    } catch (error) {
+        const { status = 500, message = "Server error" } =
+            error;
+        res.status(status).json({ message });
+    }
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = (req, res) => {
+    // removeContact,
+};
+
+export const createContact = (req, res) => {
+    // addContact,
+};
 
 export const updateContact = (req, res) => {};
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -17,21 +17,21 @@ export const getContactById = async (contactId) => {
     return contact || null;
 };
 
-export const addContact = async (name, email, phone) => {
-    const contacts = await listContacts();
-    const newContact = {
-        id: nanoid(),
-        name,
-        email,
-        phone,
-    };
-    contacts.push(newContact);
-    await fs.writeFile(
-        contactsPath,
-        JSON.stringify(contacts, null, 2)
-    );
-    return newContact || null;
-};
+// export const addContact = async (name, email, phone) => {
+//     const contacts = await listContacts();
+//     const newContact = {
+//         id: nanoid(),
+//         name,
+//         email,
+//         phone,
+//     };
+//     contacts.push(newContact);
+//     await fs.writeFile(
+//         contactsPath,
+//         JSON.stringify(contacts, null, 2)
+//     );
+//     return newContact || null;
+// };
 
 export const removeContact = async (contactId) => {
     const contacts = await listContacts();
